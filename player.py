54c671b31a9f80bf0a8acfbbe20c974dbf13e6a9
Tangedy <edtangingson@gmail.com>
2025-05-31 10:08:53 -0400

    change controls

--- player.py
+++ player.py
@@ -12,16 +12,16 @@ class Player(pygame.sprite.Sprite):
         self.obstacle_sprites = obstacle_sprites
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_w]:
             self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
+        elif keys[pygame.K_s]:
             self.direction.y = 1
         else:
             self.direction.y = 0
 
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_a]:
             self.direction.x = -1
-        elif keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_d]:
             self.direction.x = 1
         else:
             self.direction.x = 0
