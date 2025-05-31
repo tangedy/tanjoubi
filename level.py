34b7c3e66c05f976051c16d692af1faabd7ac433
Tangedy <edtangingson@gmail.com>
2025-05-31 04:24:06 -0400

    create camera and hitbox

--- level.py
+++ level.py
@@ -30,7 +30,7 @@ class Level:
 
     def run(self):
         #update and draw the game
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -38,7 +38,16 @@ class YSortCameraGroup(pygame.sprite.Group):
         #general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-    def custom_draw(self):
-        for sprite in self.sprites():
-            self.display_surface.blit(sprite.image, sprite.rect)
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(100, 200)
+    def custom_draw(self, player):
+
+        #getting the offset
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_position)
         
